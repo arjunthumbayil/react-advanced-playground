aada4a50958b4270a5d1424649a44918786473b4
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-11 11:27:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Setup from './tutorial/1-useState/setup/5-useState-counter'
+import Final from './tutorial/1-useState/final/5-useState-counter'
+
+function App() {
+  return (
+    <>
+      <section></section>
+    </>
+  )
+}
+
+export default App
---
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+const UseStateCounter = () => {
+  return <div className='container'></div>
+}
+
+export default UseStateCounter
