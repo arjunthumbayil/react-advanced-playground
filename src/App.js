e95bfb750de7e308ea1b7c6c66fd6d019330584b
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-11 15:42:22 +0530

    useEffect() basics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Setup from './tutorial/1-useState/setup/5-useState-counter'
-import Final from './tutorial/1-useState/final/5-useState-counter'
+import React from "react";
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+import Final from "./tutorial/2-useEffect/final/1-useEffect-basics";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
