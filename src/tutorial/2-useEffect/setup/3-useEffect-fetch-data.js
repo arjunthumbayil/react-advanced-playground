08ef064a118146cd8e72477a62f147f0b6145752
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-11 20:12:59 +0530

    fetchData

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h1>github users</h1>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avtar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avtar_url} alt={login}></img>
+              <div>
+                <h4>{login}</h4>
+                <a target="_blank" rel="noopener noreferrer" href={html_url}>
+                  profile
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
