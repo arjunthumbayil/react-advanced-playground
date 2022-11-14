44a58afafed1b15aa0ccb77492953dc9fe9604fc
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-14 20:39:25 +0530

    useRef()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
-import Final from "./tutorial/4-forms/final/2-multiple-inputs";
+import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
+import Final from "./tutorial/5-useRef/final/1-useRef-basics";
 
 function App() {
   return (
