48de1792cd2381e7008b5443368b31da80668cc3
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-27 20:41:43 +0530

    useReducer useState setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
-import Final from "./tutorial/5-useRef/final/1-useRef-basics";
+import Setup from "./tutorial/6-useReducer/setup/";
+import Final from "./tutorial/6-useReducer/final/";
 
 function App() {
   return (
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,46 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name + " this is coming from the input");
+    if (name) {
+      setShowModal(false);
+      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName("");
+    } else {
+      setShowModal(true);
+    }
+  };
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add_Item</button>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
