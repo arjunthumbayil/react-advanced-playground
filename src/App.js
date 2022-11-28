8cc7b2554cae2909d9f7fedb427d563f8f906ec0
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-28 12:06:40 +0530

    prop drilling example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup/";
-import Final from "./tutorial/6-useReducer/final/";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
+import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
 
 function App() {
   return (
