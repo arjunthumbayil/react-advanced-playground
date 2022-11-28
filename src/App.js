f1be5a98f999c012b071614774675be01dbf6dc4
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-28 13:07:29 +0530

    Context API Basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
-import Final from "./tutorial/7-prop-drilling/final/1-prop-drilling";
+import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Final from "./tutorial/8-useContext/final/1-context-api";
 
 function App() {
   return (
--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,7 +1,9 @@
-import React, { useState, useContext } from 'react';
-import { data } from '../../../data';
+import React, { useState, useContext } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
+
+const PersonContext = React.createContext();
 
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
@@ -11,32 +13,29 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{ removePerson, people }}>
+      <h3>Context API</h3>
+      <List people={people} />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const mainData = useContext(PersonContext);
+
   return (
     <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+      {mainData.people.map((person) => {
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext);
   return (
-    <div className='item'>
+    <div className="item">
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
